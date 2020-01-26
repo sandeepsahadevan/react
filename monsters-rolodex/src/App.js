4292c1aefce3b4c869bd8a85dd3de42895667357
sandeep <sandeep.sahadevan@cognizant.com>
2020-01-26 19:24:52 +0000

    Create a monster site

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component'
 import logo from './logo.svg';
 import './App.css';
+import { Search } from './components/search-box/search-box.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Sandeep First React Page
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters : [],
+      searchMonster : ''
+    };
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState( {searchMonster : e.target.value});
+  }
+
+  render() {
+    const { monsters, searchMonster } =this.state;
+    const  filteredMonsters  = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchMonster.toLowerCase())
+      )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Search placeholder = 'Select Monster' handleChange={this.handleChange}/>
+          <CardList monsters={filteredMonsters}/>        
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './search-box.styles.css'
+
+export const Search = ( {placeholder, handleChange}) => (
+    <input className= 'search' type='search' placeholder = {placeholder} onChange={handleChange}/>
+);
