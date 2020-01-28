812e076d44c6aac0b184ed2aeeae2540fafe54f1
sandeep <sandeep.sahadevan@cognizant.com>
2020-01-28 20:21:36 +0000

    ECOMM PROJECT

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div> Hello  
+    <div> <HomePage/>  
     </div>
   );
 }
