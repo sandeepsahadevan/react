487c29d082e527d0d4eb408429a9b4f1d042894a
SSAHADE3 <ssahade3@ford.com>
2020-01-29 15:32:05 +0000

    Routing

--- crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './menu-item.styles.scss'
 
-const MenuItem = ({title , imageUrl, id, size}) => (
+const MenuItem = ({title , imageUrl, size}) => (
     <div className={`${size} menu-item`}>
     <div
         className='background-image'
@@ -12,7 +12,7 @@ const MenuItem = ({title , imageUrl, id, size}) => (
           <div className='title'>{title.toUpperCase()}</div>
           <span className='subtitle'>Shop Now</span>
         </div>
-      </div>
+    </div>    
 );
 
 export default MenuItem;
