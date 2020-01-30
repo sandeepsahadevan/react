4163b1a5ff6bb281c6f3ff8bc5544e062343f977
sandeep <sandeep.sahadevan@cognizant.com>
2020-01-30 22:39:53 +0000

    header

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
-    <div> 
+    <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
