bf515fc26b70251078b3a8994951e3ce0cc8c400
sandeep <sandeep.sahadevan@cognizant.com>
2020-02-01 16:43:06 +0000

    signinwithgoogle

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 
 import CustomButton from '../custom-button/custom-button.component'
+import {signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component{
     
@@ -34,6 +35,9 @@ class SignIn extends React.Component{
                     <FormInput label='Email' type='text' value={this.state.email} name='email' handleChange={this.handleChange}/>                    
                     <FormInput label='Password' type='password' value={this.state.password} name='password' handleChange={this.handleChange}/>
                     <CustomButton type='submit' value='Submit Form'>SIGN IN</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        Sign in with Google
+                    </CustomButton>
                 </form>
             </div>
         );
---
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAneJ3i_Sq_yvgOUueuJ2Gzbb2SiUX5XNU",
+  authDomain: "crwn-db-d99fb.firebaseapp.com",
+  databaseURL: "https://crwn-db-d99fb.firebaseio.com",
+  projectId: "crwn-db-d99fb",
+  storageBucket: "crwn-db-d99fb.appspot.com",
+  messagingSenderId: "899742761683",
+  appId: "1:899742761683:web:e495448859794ab8b24a26",
+  measurementId: "G-WWWNDYS3S1"
+};
+
+firebase.initializeApp(config);
+
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+
+export default firebase;
