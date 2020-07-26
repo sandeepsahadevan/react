6132e637e846af6127c3c086256cce9d2bfb69bd
sandeep <sandeep.sahadevan@cognizant.com>
2020-07-26 17:01:21 +0100

    stripe integration

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H2cLQJIdBAtBLanqeZ36QnRyFBZ43WPXAYmvNWEjvI0N83ku9KJ1twlygF5pEe2gHh1ZwKyV5u1mnB0iRVSMjJb00hblrkjkr';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
